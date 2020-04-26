fdd1a802f23d9c5ef173ef2db93116d9dd1a6f95
Orlaaa29 <orlamaevereilly16@gmail.com>
2020-04-26 22:39:38 +0100

    Fixing video F
    Will delete
    Can't serve web -api

--- src/app/in-memory-database.service.ts
+++ src/app/in-memory-database.service.ts
@@ -8,12 +8,12 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDatabaseService implements InMemoryDbService {
 
   createDb(){
-    const items:AssignmentItem[]=[
+    const items:AssignmentItem[] = [
       {id:1,title:"WDF",description:"WDF",dategiven: "03/02/20",datedue: "05/05/20",percentage: 65},
       {id:2,title:"AMR",description:"AMR",dategiven: "03/02/20",datedue: "03/05/20",percentage: 65},
       {id:3,title:"DL",description:"DL",dategiven: "03/02/20",datedue: "03/05/20",percentage: 65}
     ];
 
-    return {items};
+    return { items };
   }
 }
