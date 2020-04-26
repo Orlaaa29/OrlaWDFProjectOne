2d3c580b994aa81152d70d17cbe639410408b374
Orlaaa29 <orlamaevereilly16@gmail.com>
2020-04-26 22:44:51 +0100

    Update assignment-task-service.service.ts

--- src/app/assignment-task-service.service.ts
+++ src/app/assignment-task-service.service.ts
@@ -20,24 +20,24 @@ export class AssignmentTaskServiceService {
    public getAllAssignmentItems():Observable<AssignmentItem[]>
    {
      console.log("AssignmentTaskServiceService:getAllAssignmentItems ");
-     return this.http.get<AssignmentItem>(this.webURI,this.httpOptions);
+     return this.http.get<AssignmentItem[]>(this.webURI,this.httpOptions);
    }
 
-   public getAssignmentItem(itemID:number):Observable<AssignmentItem[]>
+   public getAssignmentItem(itemID:number):Observable<AssignmentItem>
    {
      console.log("Looking for the item", itemID);
      return this.http.get<AssignmentItem>(this.webURI + "/" + itemID);
    }
 
-   public addAssignmentItem(anItem:AssignmentItem):Observable<AssignmentItem[]>{
+   public addAssignmentItem(anItem:AssignmentItem):Observable<AssignmentItem>{
     return this.http.post<AssignmentItem>(this.webURI, anItem, this.httpOptions);
    }
 
-   public deleteAssignmentItem(anItem:AssignmentItem):Observable<AssignmentItem[]>{
+   public deleteAssignmentItem(anItem:AssignmentItem):Observable<AssignmentItem>{
     return this.http.delete<AssignmentItem>(this.webURI + "/" + anItem.id, this.httpOptions);
    }
 
-  public updateAssignmentItem(updatedItem:AssignmentItem):Observable<AssignmentItem[]>{
+  public updateAssignmentItem(updatedItem:AssignmentItem):Observable<AssignmentItem>{
   return this.http.put<AssignmentItem>(this.webURI + "/" + updatedItem, this.httpOptions);
   }
  
